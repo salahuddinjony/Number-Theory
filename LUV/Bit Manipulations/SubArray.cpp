8e6b9dde99b4de0972b62554c419cbf1f722a020
SALAH <salah@SALAHs-MacBook-Pro.local>
2025-01-16 12:57:50 +0600

    added

--- LUV/Bit Manipulations/SubArray.cpp
+++ LUV/Bit Manipulations/SubArray.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void generateSubarrays(int start, int end, vector<int> &nums, int m, int &cnt) {
+void generateSubarrays(int start, int end, vector<int> &nums) {
     if (end == nums.size()) { 
         // Base case: If 'end' reaches the size of the array, stop recursion
         return;
     } else if (start > end) { 
         // Move to the next subarray by increasing 'end'
-        generateSubarrays(0, end + 1, nums, m, cnt);
+        generateSubarrays(0, end + 1, nums);
     } else {
-        // Process the current subarray
-        int sum = 0;
+        // Print the current subarray
+        cout << "Subarray: ";
         for (int i = start; i <= end; i++) {
-            sum += nums[i];
+            cout << nums[i] << " ";
         }
-        if (sum % m == 0) {
-            cnt++;
-        }
+        cout << endl;
 
         // Move to the next starting point in the current subarray
-        generateSubarrays(start + 1, end, nums, m, cnt);
+        generateSubarrays(start + 1, end, nums);
     }
 }
 
@@ -28,17 +26,14 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    int n, m;
-    cin >> n >> m;
+    int n;
+    cin >> n;  // Input the size of the array
     vector<int> nums(n);
     for (int i = 0; i < n; i++) {
         cin >> nums[i]; // Input the elements of the array
     }
 
-    int cnt = 0; // Counter for subarrays divisible by m
-    generateSubarrays(0, 0, nums, m, cnt); // Start generating subarrays from (0, 0)
-
-    cout << cnt << endl; // Output the total count
+    generateSubarrays(0, 0, nums); // Start generating subarrays from (0, 0)
 
     return 0;
 }
